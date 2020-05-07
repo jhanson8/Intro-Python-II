71fad2693b384330e8465af8e88e35fcb26efd09
Jonathan Hanson <jhanson8@uoregon.edu>
2020-05-06 18:03:42 -0700

    cardinal directions, classes

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player 
 
 # Declare all the rooms
 
@@ -38,7 +39,16 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+#new_player = Player("Jon", "outside").__str__()
+#print(new_player)
 
+name = ""
+while name == "":
+    name = input("\nEnter your name to begin your journey: ").strip()
+    
+new_player = Player(name, room['outside'])
+
+print(f"Current location: { new_player.current_room.name }")
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +59,26 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    print(f"\nCurrent location: { new_player.current_room.name }")
+    print(f"\nClue: { new_player.current_room.desc }")
+    print("\nPlease enter a cardinal direction to move in: n, s, e, or w.\nn=North, s=South, e=East, w=West")
+    input_direction = input().strip().lower().split(" ")
+    input_length = len(input_direction)
+    if input_length == 1:
+        user_input = input_direction[0]
+        if user_input == "n":
+            new_player.move_to_room(new_player.current_room.n_to)
+        elif user_input == "e":
+            new_player.move_to_room(new_player.current_room.e_to)
+        elif user_input == "s":
+            new_player.move_to_room(new_player.current_room.s_to)
+        elif user_input == "w":
+            new_player.move_to_room(new_player.current_room.w_to)
+        elif user_input == "q":
+            break
+        else:
+            print("Please enter a valid command, press '?' for commands")
+
+
--- src/player.py
+++ src/player.py
@@ -1,2 +1,23 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, current_room):
+        #super().__init__(room_name)
+        self.current_room = current_room
+        self.name = name
+       
+    def __str__(self):
+        return 'current_room: %s' % (self.current_room)
+
+    def move_to_room(self, room):
+        if room is None:
+            print("You cannot move in that direction from your current room")
+        else:
+            self.current_room = room
+
+
+
+
+
